4c7a00e757b41a8cfca51115386d0680f4d6cfcb
Jesús Romeo <jrmougan@gmail.com>
2025-01-12 18:35:35 +0100

    Activa autopilot

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -9,7 +9,7 @@ public class Drive : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public GameObject fuel;
-    bool autopilot = false;
+    bool autopilot = true;
     float tspeed = 2f;
     float rspeed = 0.2f;
 
